21ee75004f9eac1f9ee6eaa8c0fc76893dd99850
Teresa Yung <tereyung@gmail.com>
2014-10-27 16:19:28 -0700

    added comments to app.jsx

--- templates/app/client/app/src/app.jsx
+++ templates/app/client/app/src/app.jsx
@@ -1,6 +1,14 @@
 /** @jsx React.DOM */
 
 
+/*ReadMe:
+  This code is written in jsx; <this.props.activeRouteHandler/>
+  is used below for changing views for routing.  
+
+  To add components, you can run react-fullstack:component, and it will create a new component file in templates/app/client/app/src/components.
+  Components are where you will be designing your view in the render section.  
+*/
+
 'use strict';
 
 var React = require('react');
